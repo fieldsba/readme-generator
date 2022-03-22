800f122b42bc6786b15145343b319e3fcaf8cb16
Alex Fields <brooke.fields415@gmail.com>
2022-03-22 18:14:18 -0400

    added license link and badge functions

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -14,12 +14,27 @@ function renderLicenseBadge(license) {
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  
+  if (license === 'MIT') {
+    return 'https://choosealicense.com/licenses/mit/';
+  }
+  else if (license === 'GNU GPLv3') {
+    return 'https://choosealicense.com/licenses/gpl-3.0/';
+  }
+  else {
+    return "";
+  }
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === 'MIT' || license === 'GNU GPLv3') {
+    return `${license}`
+  }
+  else {
+    return "";
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
